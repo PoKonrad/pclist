b7e914d53e2d9238f3729baaf673a798194869e5
Rad <Rad@radski.eu>
2022-08-17 20:30:55 +0200

    Added data route

--- api/src/controllers/data.js
+++ api/src/controllers/data.js
@@ -6,8 +6,40 @@ const router = Router();
 
 router.use(Authentication.verifyJWT());
 
-router.get('/', Authentication.requireRole('user'), async (req, res) => {
-  await dbQuery();
-});
+router.get(
+  '/:from-:to',
+  Authentication.requireRole('user'),
+  async (req, res) => {
+    const from = parseInt(req.params.from);
+    const to = parseInt(req.params.to);
+    const resp = await dbQuery('SELECT * FROM pcs LIMIT ?,?', [from, to]);
 
+    res.status(200).json({
+      error: false,
+      data: resp,
+    });
+  }
+);
+
+router.post('/', Authentication.requireRole('admin'), async (req, res) => {
+  const postData = req.body;
+  try {
+    await dbQuery('INSERT INTO pcs (name, cpu, gpu, ram) VALUES (?, ?, ?, ?)', [
+      postData.name,
+      postData.cpu,
+      postData.gpu,
+      postData.ram,
+    ]);
+
+    res.status(201).json({
+      error: false,
+      message: 'Succesfully inserted',
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: 'An error has occured',
+    });
+  }
+});
 export default router;
